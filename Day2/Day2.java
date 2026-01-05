69ff3fa9c231e5ca5ed40f50319ffe59a7a448f1
Aleena Joseph <115602866+aleena-joseph-86@users.noreply.github.com>
2026-01-05 12:28:14 +0530

    Add files via upload

---
+++ Day2/Day2.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+class Day2 {
+  public static void main(String args[]) {
+    // Get input from the user
+    System.out.print("Enter the value of N: ");
+    Scanner scanner = new Scanner(System.in);
+    int N = scanner.nextInt();
+    scanner.close();
+
+    for(int i = 1; i <= N; i++) {
+      for(int j = 1; j <= N;  j++) {
+        System.out.print("*");
+      }
+      System.out.print("\n");
+    }
+  }
+}
